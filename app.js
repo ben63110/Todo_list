f32e9e3e550ccdb301178e5d09522bbe7aefa4e7
ben63110 <ben6311000@gmail.com>
2022-07-02 18:40:33 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.post('/todos', (req, res) => {
   const name = req.body.name
   const todo = new Todo({
